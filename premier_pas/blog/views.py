5dbb9b51b74e1d74af56abe62c90f16c706e3bd9
abdlynng <abdlynng@gmail.com>
2020-03-18 14:17:26 +0100

    ajout d'une vue a partir de views.py dans l'application blog avec la classe HttpResponse et l(objet home : rappel cette usage est non conci

--- premier_pas/blog/views.py
+++ premier_pas/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# view avec HttpResponse 
+
+def home(request):
+    return HttpResponse("""
+        <p>Bienvenue dans l'acceuil </p>
+    """)
