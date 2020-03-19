74f061c6cc40ea83c58093b33c3686f6e239247c
abdlynng <abdlynng@gmail.com>
2020-03-19 11:06:09 +0100

    ajout de la fonction view_article(request, id_article) pour passer en argument en url

--- premier_pas/blog/views.py
+++ premier_pas/blog/views.py
@@ -9,3 +9,9 @@ def home(request):
     return HttpResponse("""
         <p>Bienvenue dans l'acceuil </p>
     """)
+
+#passage par identifiant 
+def view_article(request, id_article):
+	return HttpResponse(
+		"Vous avez demandé l'article {0}".format(id_article)
+	)
